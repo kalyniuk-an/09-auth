4b6f3be4a5e9dcbb79943ccf07d178808d94fa5c
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-09-11 21:26:40 +0300

    upd api

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import { NewNote } from '@/lib/api';
+import { NewNote } from '@/lib/api/clientApi';
 
 interface NoteDraftStore {
   draft: NewNote;
